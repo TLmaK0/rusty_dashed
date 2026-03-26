5429d95a048ed05041d4e3238a353878b2e9791b
Hugo Freire <hugofreiregil@gmail.com>
2026-03-26 20:13:10 +0100

    Replace legacy deps with modern alternatives
    - iron/hyper/mount/staticfile -> tiny_http + rust-embed
    - includedir/includedir_codegen -> rust-embed (derive macro)
    - ws 0.5 -> ws 0.9
    - lazy_static 0.1 -> 1
    - rand 0.3 -> 0.8
    - Remove rustc-serialize completely
    - Update dashboard example to use serde
    - Bump version to 0.3.0

--- src/ws_server.rs
+++ src/ws_server.rs
@@ -1,20 +1,25 @@
-use std::sync::mpsc::{Sender, channel};
+use std::sync::mpsc::{channel, Sender};
 use std::sync::Mutex;
-use std::thread::{spawn};
-use ws::{Handler as WsHandler, WebSocket};
+use std::thread::spawn;
+use ws::{Handler as WsHandler, Handshake, Result as WsResult};
 
-pub struct WsServer {
-}
+pub struct WsServer;
 
 lazy_static! {
     static ref SENDER: Mutex<Sender<String>> = Mutex::new(WsServer::start_ws());
 }
 
+struct WsHandler_;
+
+impl WsHandler for WsHandler_ {
+    fn on_open(&mut self, _: Handshake) -> WsResult<()> {
+        Ok(())
+    }
+}
+
 impl WsServer {
     fn start_ws() -> Sender<String> {
-        let web_socket = WebSocket::new(|_| {
-            WsServer{}
-        }).unwrap();
+        let web_socket = ws::WebSocket::new(|_| WsHandler_).unwrap();
         let broadcast = web_socket.broadcaster();
 
         spawn(move || {
@@ -23,22 +28,14 @@ impl WsServer {
 
         let (tx, rx) = channel();
 
-        spawn(move || {
-            loop {
-                let message = rx.recv().unwrap();
-                broadcast.send(message).unwrap();
-            }
+        spawn(move || loop {
+            let message = rx.recv().unwrap();
+            let _ = broadcast.send(message);
         });
         tx
     }
 
-    pub fn send_message(message: String){
-        //TODO: remove this lock
+    pub fn send_message(message: String) {
         SENDER.lock().unwrap().clone().send(message).unwrap();
     }
 }
-
-impl WsHandler for WsServer {
-}
-
-
